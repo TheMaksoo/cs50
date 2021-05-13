f3b720ba06d0bbdc7080527bfc0f5f5655309062
TheMaksoo <39918032+Moheax@users.noreply.github.com>
2021-05-14 00:07:15 +0200

    Add files via upload

--- addition.c
+++ addition.c
@@ -3,9 +3,17 @@
 
 int main(void)
 {
-    printf("Type a number for x and y.\n");
+    // 32 bit
+    printf("\nType a number for x and y.\n");
     int x = get_int("x: ");
     int y = get_int("y: ");
 
     printf("\n%i + %i = %i\n", x, y, x + y);
+
+    // 64 bit
+    printf("\nType a big number for x and y.\n");
+    long z = get_long("x: ");
+    long q = get_long("y: ");
+
+    printf("\n%li + %li = %li\n", z, q, z + q);
 }
