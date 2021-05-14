1d3f1fea0c5a5aed01491fb66f5861510fcf4f2c
TheMaksoo <39918032+Moheax@users.noreply.github.com>
2021-05-14 18:00:57 +0200

    Add files via upload

---
+++ Week1/addition.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    // 32 bit
+    printf("\nType a number for x and y. for addition.\n");
+    int x = get_int("x: ");
+    int y = get_int("y: ");
+
+    printf("\n%i + %i = %i\n", x, y, x + y);
+
+    // 64 bit
+    printf("\nType a big number for x and y.\n");
+    long z = get_long("x: ");
+    long q = get_long("y: ");
+
+    printf("\n%li + %li = %li\n", z, q, z + q);
+}
