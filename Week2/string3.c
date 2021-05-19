6413a2058c71a39fff8a6555ba12d12a7e838a6a
TheMaksoo <39918032+Moheax@users.noreply.github.com>
2021-05-19 22:04:31 +0200

    Add files via upload

---
+++ Week2/string3.c
@@ -0,0 +1,13 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    string s = get_string("Input:  ");
+    printf("Output: ");
+    for (int i = 0; s[i] != '\0'; i++)
+    {
+        printf("%c", s[i]);
+    }
+    printf("\n");
+}
