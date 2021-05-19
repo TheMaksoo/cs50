b9e675f87115f72ae545b074a610b183ca9d3e16
TheMaksoo <39918032+Moheax@users.noreply.github.com>
2021-05-19 12:24:03 +0200

    Add files via upload

---
+++ Week2/hello.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    string awnser = get_string("What's your name?\n");
+    printf("Hello, %s\n", awnser);
+
+}
+
+//to compile type "Make [FILENAME]"
+//to run the type "./[FILENAME]"
